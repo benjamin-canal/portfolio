d81b14a9c83cc827aab95f1b8ce74aecda218da3
benjamin-canal <benjamin.canal@gmail.com>
2022-07-21 01:02:29 +0200

    constraints OK

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -5,19 +5,32 @@ namespace App\Form;
 use App\Entity\ContactEmail;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('subject')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('text')
-            //->add('createdAt')
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Objet de l\'email',
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Votre prénom',
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Votre nom',
+            ])
+            ->add('text', TextareaType::class, [
+                'label' => 'Votre message',
+            ])
         ;
     }
 
