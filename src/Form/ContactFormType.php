40ecd55e98e3df1d26b15033c6b6d69bbe2a1100
benjamin-canal <benjamin.canal@gmail.com>
2022-07-20 22:38:58 +0200

    reCaptcha V3 OK

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\ContactEmail;
 use Symfony\Component\Form\AbstractType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 
 class ContactFormType extends AbstractType
 {
@@ -18,6 +20,10 @@ class ContactFormType extends AbstractType
             ->add('lastName')
             ->add('text')
             //->add('createdAt')
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
+            ]);
         ;
     }
 
