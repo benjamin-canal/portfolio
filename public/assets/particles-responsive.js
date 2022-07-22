66f573082a68aaad8e1efb31beaa1e3a60d3cd85
benjamin-canal <benjamin.canal@gmail.com>
2022-07-22 03:39:12 +0200

    update responsive particles

--- public/assets/particles-responsive.js
+++ public/assets/particles-responsive.js
@@ -3,9 +3,97 @@ const options = {
         /* omissis, this is the main options object */
         responsive: [
             {
-                maxWidth: 599,
+                maxWidth: 500,
                     options: { 
                         interactivity: {
+                        events: {
+                            onHover: {
+                                enable: true,
+                                mode: "repulse"
+                            },
+                        },
+                        modes: {
+                            repulse: {
+                                distance: 80,
+                            },
+                        },
+                    },
+                    particles: {
+                        //color: {
+                        //    value:["#C31997","#43E54B","#43E5E3","#c905d6"]
+                        //},
+                        links: {
+                            enable: true,
+                            opacity: 0.5,
+                            distance: 200,
+                            color: "#32F7D6",
+                        },
+                        number: {
+                            value: 25,
+                          },
+                        move: {
+                            enable: true,
+                            speed: { min: 0.2, max: 0.8 },
+                        },
+                        opacity: {
+                            value: 0, //{ min: 0.2, max: 1.5 },
+                        },
+                        size: {
+                            value: { min: 0.5, max: 5 },
+                        }, 
+                    },
+                },
+        
+            },
+            
+            {
+                maxWidth: 649,
+                    options: { 
+                        interactivity: {
+                        events: {
+                            onHover: {
+                                enable: true,
+                                mode: "repulse"
+                            },
+                        },
+                        modes: {
+                            repulse: {
+                                distance: 80,
+                            },
+                        },
+                    },
+                    particles: {
+                        //color: {
+                        //    value:["#C31997","#43E54B","#43E5E3","#c905d6"]
+                        //},
+                        links: {
+                            enable: true,
+                            opacity: 0.5,
+                            distance: 200,
+                            color: "#32F7D6",
+                        },
+                        number: {
+                            value: 40,
+                          },
+                        move: {
+                            enable: true,
+                            speed: { min: 0.2, max: 0.8 },
+                        },
+                        opacity: {
+                            value: 0, //{ min: 0.2, max: 1.5 },
+                        },
+                        size: {
+                            value: { min: 0.5, max: 5 },
+                        }, 
+                    },
+                },
+        
+            }, 
+
+            {
+                maxWidth: 699,
+                options: { 
+                    interactivity: {
                         events: {
                             onHover: {
                                 enable: true,
@@ -43,10 +131,10 @@ const options = {
                         }, 
                     },
                 },
-        
-            }, 
+            },
+
             {
-                minWidth: 600,
+                minWidth: 700,
                 options: { 
                     interactivity: {
                         events: {
@@ -72,7 +160,7 @@ const options = {
                             color: "#32F7D6",
                         },
                         number: {
-                            value: 75,
+                            value: 80,
                           },
                         move: {
                             enable: true,
@@ -86,7 +174,8 @@ const options = {
                         }, 
                     },
                 },
-            }],
+            },
+        ],
 };
 
 tsParticles.load("tsparticles", options);
